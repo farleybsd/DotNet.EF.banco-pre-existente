2347ad5a0421cde4a29d96f39b87df04128a6052
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-04-11 15:50:01 -0300

    Definindo valores para shadow properties

--- Alura.Filmes/Alura.Filmes.App/Program.cs
+++ Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.Filmes.App.Dados;
 using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
 using System;
 
 namespace Alura.Filmes.App
@@ -14,10 +15,21 @@ namespace Alura.Filmes.App
             {
                 //contexto.LogSQLToConsole();
 
-                foreach (var ator in contexto.Atores)
-                {
-                    Console.WriteLine(ator);
-                }
+                //foreach (var ator in contexto.Atores)
+                //{
+                //    Console.WriteLine(ator);
+                //}
+
+                // Insert  Shadow Protetys
+                var ator = new Ator();
+                ator.PrimeiroNome = "Farley";
+                ator.UltimoNome = "Rufino";
+                //Shadow Protetys
+                contexto.Entry(ator).Property("last_update").CurrentValue = DateTime.Now;// Colocando um valor obrigario para o banco 
+                                                                                        //  mais que nao e importante para o negocio
+
+                contexto.Atores.Add(ator);
+                contexto.SaveChanges();
             }
 
             Console.ReadKey();
