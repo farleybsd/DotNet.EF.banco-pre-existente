1788e28491e6235d166bd395be353f1259deaeb4
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-04-12 23:59:25 -0300

    Criando índices com o Entity

---
+++ DotNet.EF.Banco.Legado.PTII/Alura.Filmes.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+               
+
+            }
+        }
+    }
+}
