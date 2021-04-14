a6738df3c8b7dc8e4161b4908108af02eb0d0bb9
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-04-14 01:39:40 -0300

    FromSql e suas limitações

--- DotNet.EF.Banco.Legado.PTII/Alura.Filmes.App/Program.cs
+++ DotNet.EF.Banco.Legado.PTII/Alura.Filmes.App/Program.cs
@@ -42,20 +42,24 @@ namespace Alura.Filmes.App
 
 
                 
-                var sql = @"SELECT TOP 5
-                                    A.first_name,
-                                    A.last_name,
-                                    COUNT(*) AS TOTAL
-                            FROM actor A
-	                                INNER JOIN film_actor FA ON FA.actor_id = A.actor_id
-                            GROUP BY 
-                                    A.first_name,
-                                    A.last_name
-                            ORDER BY 
-                                    TOTAL
+                var sql = @"SELECT
+                                    a.*
+                            FROM actor a
+	                            inner join (
+                                            SELECT TOP 5
+                                                        A.actor_id,
+                                                        COUNT(*) AS TOTAL
+                                            FROM actor a
+	                                                    INNER JOIN film_actor FA ON FA.actor_id = a.actor_id
+                                            GROUP BY 
+                                            A.actor_id
+                                            ORDER BY TOTAL desc) filmes on filmes.actor_id = a.actor_id
                             ";
 
-                var  atoresMaisAtuantes = contexto.Atores.FromSql(sql);
+                var atoresMaisAtuantes = contexto
+                                                .Atores
+                                                .FromSql(sql)
+                                                .Include(a => a.Filmografia);
 
                 foreach (var ator in atoresMaisAtuantes)
                 {
--- DotNet.EF.Banco.Legado.PTII/QUERYEF.sql
+++ DotNet.EF.Banco.Legado.PTII/QUERYEF.sql
@@ -8,3 +8,19 @@ GROUP BY
 A.first_name,
 A.last_name
 ORDER BY TOTAL
+
+
+-- modelo ef 
+
+SELECT
+a.*
+FROM actor a
+	inner join (
+SELECT TOP 5
+A.actor_id,
+COUNT(*) AS TOTAL
+FROM actor a
+	INNER JOIN film_actor FA ON FA.actor_id = a.actor_id
+GROUP BY 
+A.actor_id
+ORDER BY TOTAL desc) filmes on filmes.actor_id = a.actor_id
